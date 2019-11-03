4d38bdbca722e4323d0a9371510f0d1dfd5591df
Melkam Beyene <melkam3392beyu@gmail.com>
2019-11-03 06:42:11 -0500

    modified on app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+ app.get('/',(req,res)=>{
+  res.send("working");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
